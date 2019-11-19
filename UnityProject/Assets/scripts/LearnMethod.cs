2d02af1dd071efe94dde46bb08107b9603efb792
q1e2t3y45 <57486297+q1e2t3y45@users.noreply.github.com>
2019-11-19 20:58:44 +0800

    20191119

--- UnityProject/Assets/scripts/LearnMethod.cs
+++ UnityProject/Assets/scripts/LearnMethod.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class LearnMethod : MonoBehaviour
 {
-    void Drive()
+    void Drive(int speed = 0, string direction = "北", string gear = "D")
     {
+        print("車子正在往" + direction + "開，時速:" + speed + "，" + gear + "檔");
+    }
 
+    private void Start()
+    {
+        Drive();
+        Drive(10, "東");
+        Drive(20, gear: "R");
     }
 }
